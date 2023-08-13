e442b965113d229eb8bd525754d9a44f1afd7bb4
Хатика <Venom_Skill@outlook.com>
2023-08-14 06:41:13 +0700

    web

--- completion_games-v.1.0.py
+++ completion_games-v.1.0.py
@@ -21,7 +21,7 @@ def filling_db():
         print("Название игры: " + str(get_game_name(aid[i])))
         print("Описание игры: " + str(get_game_description(aid[i])))
         print("Релизная дата: " + str(get_release_date(aid[i])))
-        print("Жанр игры: " + str(get_game_genre(get_game_name(aid[i]))))
+        print("Оценка игры: " + str(get_game_reviews(aid[i])))
         print(" ")
 
 
@@ -63,22 +63,18 @@ def get_release_date(aid):
     months = {'янв.': '01', 'фев.': '02', 'мар.': '03',
               'апр.': '04', 'май': '05', 'июн.': '06', 'июл.': '07',
               'авг.': '08', 'сен.': '09', 'окт.': '10',
-              'ноб.': '11', 'дек.': '12'}
+              'ноя.': '11', 'дек.': '12'}
     month_index = months.get(date[1])
     date[1] = date[1].replace(date[1], month_index)
     return '/'.join(date)
 
 
-def get_game_genre(game_name):
-    game_name = game_name.replace("'", "")
-    url = 'https://en.wikipedia.org/wiki/' + str(game_name)
-    print(url)
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    genre = soup.fin('th', string='Genre(s)')
-    genre = genre.find_next('td')
-    genre = [a.get_text() for a in genre.find_all('a')]
-    return genre
+def get_game_reviews(aid):
+    url = 'https://store.steampowered.com/app/' + str(aid)
+    response = requests.get(url, headers={'Accept-Language': 'ru-RU'})
+    soup = BeautifulSoup(response.text, "lxml")
+    game_review = soup.find('span', class_='game_review_summary')
+    return game_review.text
 
 api_key = "39864BB6D6F58565CFF414BD1280495D"
 filling_db()
